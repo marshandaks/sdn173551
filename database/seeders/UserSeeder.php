0e0533eb5d7113833628c13e35d7d5a63fd32855
canda <marshanda.ksh@gmail.com>
2024-04-28 21:47:57 +0700

    crud kelar

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds
+     */
+    public function run(): void
+    {
+        $user = [
+            'name' =>'Marshanda',
+            'email' =>  'SDN173551@gmail.com',
+            'password' => bcrypt('sdn173551'),
+        ];
+
+        User::insert($user);
+
+    }
+}
